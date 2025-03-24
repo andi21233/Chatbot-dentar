cabfb4c00228b7b9714f0dff43a1fe04d04b413a
andi21233 <94105064+andi21233@users.noreply.github.com>
2025-03-24 03:15:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def chatbot():
         response = get_treatment_info(user_input)
     elif "mi-am pus implant dentar" in user_input or "după detartraj" in user_input:
         response = get_post_treatment_info(user_input)
-elif "ma doare o masea" in user_input or "durere masea" in user_input or "masea" in user_input:
+elif "ma doare o masea" in user_input:
     response = "O durere de masea poate indica o carie sau o infectie. Va recomandam o consultatie. Puteti solicita o programare direct aici."
 
 elif "detartraj" in user_input:
