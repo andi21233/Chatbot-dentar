6ef6c26836dfb20d412838abb6118860cdf08c99
andi21233 <94105064+andi21233@users.noreply.github.com>
2025-03-24 02:42:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 import datetime
 import smtplib
 from email.mime.text import MIMEText
-
+from flask_cors import CORS
+app = Flask(__name__)
 CORS(app)
-
 database = {
     "patients": {},
     "appointments": [],
